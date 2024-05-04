8dc78877374b1ba1db03830cbb965f359a7c6a8d
Takdanai <jozaza07@gmail.com>
2024-05-04 17:46:04 +0700

    Initial Commit

---
+++ src/main/java/com/takdanai/courseware/OpenCoursewareApplication.java
@@ -0,0 +1,13 @@
+package com.takdanai.courseware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCoursewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenCoursewareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/takdanai/courseware/OpenCoursewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takdanai.courseware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCoursewareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
