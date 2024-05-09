5e9437f6a7fb67c35c7780cdc4c00f8bf37245a7
Takdanai <jozaza07@gmail.com>
2024-05-09 21:27:43 +0700

    adding index controller

--- src/main/java/com/takdanai/courseware/controllers/IndexController.java
+++ src/main/java/com/takdanai/courseware/controllers/IndexController.java
@@ -1,14 +1,49 @@
 package com.takdanai.courseware.controllers;
 
+import com.takdanai.courseware.entities.Course;
+import com.takdanai.courseware.entities.Topic;
+import com.takdanai.courseware.services.CourseService;
+import com.takdanai.courseware.services.LectureService;
+import com.takdanai.courseware.services.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
-public class IndexController {
+public record IndexController(CourseService courseService, LectureService lectureService,
+                              StudentService studentService) {
 
     @GetMapping({"", "/"})
-    public String index() {
+    public String index(Model model) {
+        List<Course> courses = courseService.all();
+        List<Topic> topics = courseService.allTopics().stream().limit(6).toList();
+
+        model.addAttribute("courses", courses);
+        model.addAttribute("topics", topics);
+
         return "index";
+    }
+
+    @GetMapping("/upload")
+    public String upload() {
+        return "courses/upload";
+    }
+
+    @GetMapping("/video")
+    public String video() {
+        return "courses/video";
+    }
+
+    @GetMapping("/follow")
+    public String follow() {
+        return "students/follow";
+    }
+
+    @GetMapping("/history")
+    public String history() {
+        return "students/history";
     }
 
 }
