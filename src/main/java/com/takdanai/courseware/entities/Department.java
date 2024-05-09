ef456432f0d78d0d5d7203ab75bf8dd79902f198
Takdanai <jozaza07@gmail.com>
2024-05-09 21:17:25 +0700

    adding course list to department model

--- src/main/java/com/takdanai/courseware/entities/Department.java
+++ src/main/java/com/takdanai/courseware/entities/Department.java
@@ -1,11 +1,15 @@
 package com.takdanai.courseware.entities;
 
+import com.google.common.collect.Lists;
 import com.takdanai.courseware.entities.base.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -14,4 +18,7 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 public class Department extends BaseEntity {
     private String title;
+
+    @ManyToMany
+    private List<Course> courses = Lists.newLinkedList();
 }
