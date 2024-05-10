07f711a1cb695f31e7987d07504da0a757b678e2
Takdanai <jozaza07@gmail.com>
2024-05-10 22:43:42 +0700

    adding course request validations

--- src/main/java/com/takdanai/courseware/controllers/payload/requests/CourseRequest.java
+++ src/main/java/com/takdanai/courseware/controllers/payload/requests/CourseRequest.java
@@ -2,10 +2,14 @@ package com.takdanai.courseware.controllers.payload.requests;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import com.takdanai.courseware.controllers.payload.validators.Image;
 import com.takdanai.courseware.entities.Course;
 import com.takdanai.courseware.entities.Department;
 import com.takdanai.courseware.entities.Topic;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,24 +18,30 @@ import java.util.Set;
 
 @Data
 public class CourseRequest {
-    @NotBlank(message = "title can't be blank")
+    @NotBlank(message = "Title can't be blank.")
     public String title;
 
-    @NotBlank(message = "overview can't be blank")
+    @NotBlank(message = "Overview can't be blank.")
     public String overview;
 
     public Set<Topic> topics = Sets.newHashSet();
 
     public Set<Department> departments = Sets.newHashSet();
 
+    @Image(message = "You must insert only image type.")
     public MultipartFile thumbnail;
 
+    @NotNull(message = "Type can't be blank.")
     public Course.Type type;
 
+    @NotNull(message = "Level can't be blank.")
     public Course.Level level;
 
+    @NotNull(message = "Status can't be blank.")
     public Course.Status status;
 
+    @Valid
+    @NotEmpty(message = "Lectures can't be empty.")
     public List<LectureRequest> lectures = Lists.newLinkedList();
 
     public Course.Type[] types() {
